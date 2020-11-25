b9f2d8c1f90e3245af5c80a0211dfd99d872037b
Robert Keith <robertfkeith@me.com>
2020-11-25 15:36:07 -0800

    remove the random selections

--- test/rf_SUITE.erl
+++ test/rf_SUITE.erl
@@ -16,7 +16,7 @@ all() -> [
          ].
 
 init_per_suite(Config) ->
-    {ok, IoDevice} = file:open("../../../../test/rf.csv", [read]),
+    {ok, IoDevice} = file:open("../../../../test/shuffled_rf.csv", [read]),
     ct:pal("iodevice ~p", [IoDevice]),
     DisplayRow = fun({newline, ["pos"|_]}, Acc) ->
                          %% ignore header
@@ -41,10 +41,9 @@ train_random_supervised(Config) ->
                                             random_seed => [209,162,182,84,44,167,62,240,152,122,118,154,48,208,143,84,
                                                              186,211,219,113,71,108,171,185,51,159,124,176,167,192,23,245]}),
 
-    _ = rand:seed(exsp, {901324200495623, 139888701234, 23499291264}),
-    {Supervised, Unsupervised} = lists:partition(fun(_) -> rand:uniform(100) < 90 end, shuffle(Samples)),
+    {Supervised, Unsupervised} = lists:split(1800, Samples),
     {SupervisedSamples, SupervisedClasses} = lists:unzip(Supervised),
-    som:train_random_supervised(SOM, SupervisedSamples, SupervisedClasses, 2000),
+    som:train_random_supervised(SOM, SupervisedSamples, SupervisedClasses, 3000),
     Matched = lists:foldl(fun({Sample, Class}, Acc) ->
                                   case som:winner_vals(SOM, Sample) of
                                       {_, Class} ->
@@ -59,7 +58,7 @@ train_random_supervised(Config) ->
                           end, 0, Unsupervised),
     ct:pal("matched ~p/~p => ~p%", [Matched, length(Unsupervised), Matched / length(Unsupervised) * 100]),
     %% uncomment if you want to see the outputs
-    {ok, Serialized} = som:to_json(SOM),
+    {ok, Serialized} = som:export_json(SOM),
     %ct:pal("serialized ~p", [Serialized]),
     {ok, _Deserialized} = som:from_json(Serialized),
     %ct:pal("deserialized ~p", [Deserialized]),
@@ -73,4 +72,5 @@ to_num(String) ->
     end.
 
 shuffle(List) ->
+    _ = rand:seed(exsp, {901324200495623, 139888701234, 23499291264}),
     [X || {_,X} <- lists:sort([{rand:uniform(), N} || N <- List])].
